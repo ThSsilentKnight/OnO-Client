91971d5ae19b4104bcdf359ba85a9dd62b599298
TheSilentKnight <Reubenvillaronga@gmail.com>
2026-03-14 22:48:21 -0400

    desktop turn indicator

--- src/network/socket.ts
+++ src/network/socket.ts
@@ -3,7 +3,6 @@ import {
   displayWarning,
   getClientId,
   getRoomId,
-  mapPlayerVisuals,
 } from "../utils/helpers.js";
 import {
   boardUpdateResponse,
@@ -19,8 +18,8 @@ import {
   validateRoom,
 } from "./requestResponse.js";
 import { requestRejoinRoom } from "./requests.js";
-//export const ws = new WebSocket(`wss://api.silentknightssh.com`);
-export const ws = new WebSocket("ws://localhost:8080");
+export const ws = new WebSocket(`wss://api.silentknightssh.com`);
+//export const ws = new WebSocket("ws://localhost:8080");
 
 ws.addEventListener("open", async () => {
   connectClientSetUp();
